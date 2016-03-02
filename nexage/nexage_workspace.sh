b57125092acd0ac199aacadd32fd829d52df2645
Javier Berrocal <javier.berrocal@teamaol.com>
2016-03-02 13:50:51 +0000

    Added nexage_env file

--- nexage/nexage_workspace.sh
+++ nexage/nexage_workspace.sh
@@ -13,3 +13,5 @@ echo ${user_password} | sudo -S chgrp -R $ENV_USER $WORKSPACE
 echo ${user_password} | sudo -S mkdir -p ${WORKSPACE}/installs
 echo ${user_password} | sudo -S chown -R ${ENV_USER} ${WORKSPACE}/installs
 echo ${user_password} | sudo -S chgrp -R $ENV_USER ${WORKSPACE}/installs
+
+cp -r ./nexage/installs/ ${WORKSPACE}/installs/
